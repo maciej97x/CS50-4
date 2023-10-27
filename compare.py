ea233c4c2889048fdfcd39f48db38ee26899f288
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-27 14:45:53 +0000

    Fri, Oct 27, 2023, 2:45 PM +00:00

--- compare.py
+++ compare.py
@@ -1,11 +1,9 @@
-from cs50 import get_int
-
-x = get_int("x: ")
-y = get_int("y: ")
+x = int(input("x: "))
+y = int(input("y: "))
 
 if x < y:
     print("x is less than y")
 elif x > y:
     print("x i greater than y")
 else:
-print("x i equal to y")
+    print("x i equal to y")
