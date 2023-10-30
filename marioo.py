88b25ba18976654c10a87b123c0439d5404df734
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-30 11:07:15 +0000

    Mon, Oct 30, 2023, 11:07 AM +00:00

--- marioo.py
+++ marioo.py
@@ -1,24 +1,11 @@
-from cs50 import get_int
+print('Let build a Mario pyramid! Buy how high? Give me a positive int between 1 and 24: ', end='')
+while True:
+    height = int(input())
+    if height < 1 or height > 24:
+        print('Nah! Give me an int between 1 and 24!')
+    else:
+        break
 
-
-def get_height(min=1, max=8):
-    """Prompt user for height value."""
-    while True:
-        height = get_int("Height: ")
-        if height >= min and height <= max:
-            return height
-
-
-def print_pyramid(n):
-    """Print n height of half-pyramid to console."""
-    for i in range(1, n+1):
-        print(" "*(n-i) + "#"*i + " "*2 + "#"*i)
-
-
-def main():
-    height = get_height()
-    print_pyramid(height)
-
-
-if __name__ == "__main__":
-    main()
+# print the pyramid (semi-readable one liner)
+for row in range(height+1):
+    print(' ' * (height - row) + '#' * row + '  ' + '#' * row)
