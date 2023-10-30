6eef8ee3c46dcacc7195cc1d0784fb448363b747
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-30 06:13:58 +0000

    Mon, Oct 30, 2023, 6:13 AM +00:00

---
+++ mario.py
@@ -0,0 +1,13 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+
+def get_height():
+    while true:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                return n
+        except ValueError:
