8cde9bfe95838ff923a0439c3c1ab292b9c451ea
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-30 11:13:22 +0000

    Mon, Oct 30, 2023, 11:13 AM +00:00

--- marioo.py
+++ marioo.py
@@ -1,11 +1,9 @@
 from cs50 import get_int
 
-
 while True:
     n = get_int("Height: ")
     if n > 0 and n < 9:
         break
-
 
 for i in range(0, n, 1):
     for j in range(0, n+i+3, 1):
@@ -13,4 +11,4 @@ for i in range(0, n, 1):
             print(" ", end="")
         else:
             print("#", end="")
-        print()
+     print()
