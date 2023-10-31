6d497024cb59e39de944feb6ff1c7f5bbf7f7a26
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-31 15:16:24 +0000

    Tue, Oct 31, 2023, 3:16 PM +00:00

--- mariooo.py
+++ mariooo.py
@@ -4,7 +4,7 @@ while True:
         break
 
 for i in range(2, n, 1):
-    for j in range (0, n, 1):
+    for j in range (2, n, 1):
         if (i+j < n - 1):
             print(" ", end="")
         else:
