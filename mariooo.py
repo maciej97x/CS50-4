c21d1275c065bd431ff1e1614cc2127cc495ece1
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-31 15:14:47 +0000

    Tue, Oct 31, 2023, 3:14 PM +00:00

--- mariooo.py
+++ mariooo.py
@@ -3,7 +3,7 @@ while True:
     if n > 0 and n < 9:
         break
 
-for i in range(0, n, 1):
+for i in range(2, n, 1):
     for j in range (0, n, 1):
         if (i+j < n - 1):
             print(" ", end="")
