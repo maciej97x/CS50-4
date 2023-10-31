8422be2462ab70e0adb01d392f61e889efc60201
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-31 08:42:50 +0000

    Tue, Oct 31, 2023, 8:42 AM +00:00

---
+++ mariooo.py
@@ -0,0 +1,12 @@
+while True:
+    n = int(input("Height: "))
+    if n > 0 and n < 9:
+        break
+
+for i in range(0, n, 1):
+    for j in range (0, n, 1):
+        if (i+j < n-1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
