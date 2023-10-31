81ac8d487b6361f85ddfdf0f260f47d99d5016b3
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-31 15:55:30 +0000

    Tue, Oct 31, 2023, 3:55 PM +00:00

--- marioo.py
+++ marioo.py
@@ -5,7 +5,7 @@ while True:
 
 for i in range(0, n, 1):
     for j in range (0, n+i+3, 1):
-        if (j == n or j == n+1 or i+j < n-1):
+        if (j == n or i+j < n-1):
             print(" ", end="")
         else:
             print("#", end="")
