ffc777a8379268a6858211f09fef201beec46572
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-31 18:21:10 +0000

    Tue, Oct 31, 2023, 6:21 PM +00:00

--- bank.py
+++ bank.py
@@ -1,3 +1,11 @@
 ask = input("ask: ")
+answer1 = ("hello")
+anwer2 = ("h")
 
-ask = "h"
+while
+if ask = answer
+    print("0")
+elif x > y:
+    print("x i greater than y")
+else:
+    print("x i equal to y")
