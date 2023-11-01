0e2ddc6a69377efa8994db20c50222262f89d219
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-01 18:31:21 +0000

    Wed, Nov 1, 2023, 6:31 PM +00:00

--- fav1.py
+++ fav1.py
@@ -2,6 +2,13 @@ import csv
 
 with open("2018.csv", "r") as file:
     reader = csv.DictReader(file)
-    counts{}
+    counts = {}
     for row in reader:
-        print(row["rating"])
+        best = row["rating"]
+        if best in row:
+            counts[best] += 1
+        else:
+            counts[best] = 1
+
+for best in counts:
+    print(f"{best}: {counts[best]}")
