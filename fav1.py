719afb989c2040793cedbd5586124a4005a57269
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-02 10:59:08 +0000

    Thu, Nov 2, 2023, 10:59 AM +00:00

--- fav1.py
+++ fav1.py
@@ -9,6 +9,8 @@ with open("2018.csv", "r") as file:
             counts[best] += 1
         else:
             counts[best] = 1
+def get_value(team):
+    return counts[team]
 
 for best in counts:
     print(f"{best}: {counts[best]}")
