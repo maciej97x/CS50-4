83ba2e6b8b5818feb0a8e978e8191777df69bcbb
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-03 11:16:19 +0000

    Fri, Nov 3, 2023, 11:16 AM +00:00

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ from cs50 import SQL
 
 db = SQL("sqlite:///roster.db")
 
+roster = input("State: ")
+
+rows = db.execute("SELECT * FROM roaster WHERE State Code = 'BDS'")
+
+for row in rows:
+    print(row["State"])
+
