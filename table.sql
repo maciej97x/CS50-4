99bf9aebefb634f91854853cb2f0431884ea84b8
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-20 08:12:58 +0000

    Mon, Nov 20, 2023, 8:12 AM +00:00

---
+++ table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user(
+    id int auto_increment primary key,
+
+    surname varchar(255) not null,
+
+    username varchar(255) not null,
+
+    password varchar(255) not null,
+
+    email varchar(255) not null
+);
