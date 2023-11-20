b002e8bf88b559284718c8c6b480f31147510ecc
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-20 10:01:39 +0000

    Mon, Nov 20, 2023, 10:01 AM +00:00

---
+++ sql.sql
@@ -0,0 +1,37 @@
+create table "user"
+
+(
+
+id int auto_increment primary key,
+
+surname varchar(255) not null,
+
+username varchar(255) not null,
+
+password varchar(255) not null,
+
+email varchar(255) not null
+
+);
+
+
+
+
+
+create table request_log
+
+(
+
+id         int auto_increment primary key,
+
+action_url varchar(600) null,
+
+time       int          null,
+
+user_agent varchar(255) null,
+
+user_id    int          null,
+
+ip         varchar(60)  null
+
+);
