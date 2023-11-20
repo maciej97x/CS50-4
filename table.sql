8a145a24bb58d129c233fb6f77401b72a308680a
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-20 08:50:13 +0000

    Mon, Nov 20, 2023, 8:50 AM +00:00

--- table.sql
+++ table.sql
@@ -9,18 +9,3 @@ CREATE TABLE "user"(
 
     "email" varchar(255) not null
 );
-
-create table "request_log"(
-    "id"         int "auto_increment" primary key,
-
-    "action_url" varchar(600) null,
-
-    time       int          null,
-
-    "user_agent" varchar(255) null,
-
-    "user_id"    int          null,
-
-    "ip"         varchar(60)  null
-
-);
